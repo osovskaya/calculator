71bbc84c62abe5eb30e71bfd96ae05a6206d3273
Tatyana Osovskaya <tanya.osovskaya@mail.ru>
2016-07-05 00:01:18 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -8,6 +8,16 @@ set_exception_handler(function ($exception)
 });
 
 $calculator = new Calculator;
+$calculator->setLogger(new class {
+
+        public function log($fileName, $operator, $result, $operand1, $operand2 = '')
+        {
+            $fp = fopen($fileName, 'a+');
+            fwrite($fp, date('r').' '.$operator.' '.$operand1.' and '.$operand2.', result '.$result."\n");
+            fclose($fp);
+            return true;
+        }
+    });
 
 echo $calculator->add(3, 2).PHP_EOL;
 echo $calculator->subtract(3, 2).PHP_EOL;
